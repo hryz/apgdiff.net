a679e7bc9fab12d769092c0ffa5fa552b5f6516b
Anton Hryzodub <anton.grizodub@gmail.com>
2018-06-16 21:35:19 +0300

    corrected readme, updated the framework version

--- src/pgdiff/Properties/Resources.Designer.cs
+++ src/pgdiff/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace pgdiff.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -70,7 +70,7 @@ namespace pgdiff.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Cannot find object &apos;{0}&apos; for statement &apos;{1}&apos;..
+        ///   Looks up a localized string similar to Cannot find obj &apos;{0}&apos; for statement &apos;{1}&apos;..
         /// </summary>
         internal static string CannotFindObject {
             get {
